45a2e3745458ef85fc4e3924330a702ce4186203
Jonathan Hammond <hammond887@googlemail.com>
2024-01-15 17:12:05 +0000

    submittable data

--- ReactDemo/Controllers/HomeController.cs
+++ ReactDemo/Controllers/HomeController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ReactDemo.Models;
 
 namespace ReactDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> _comments;
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "burg",
+                    Text = "burgey burgason"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "flurg",
+                    Text = "flurgey flurason"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "turg",
+                    Text = "turgey turgason"
+                },
+            };
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Comments()
+        {
+            return Json(_comments);
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [Route("comments/new")]
+        [HttpPost]
+        public IActionResult AddComment(CommentModel comment)
+        { 
+            //create a fake ID for this comment
+            comment.Id = _comments.Count + 1;
+            _comments.Add(comment);
+            return Content("Success");
         }
     }
 }
--- ReactDemo/wwwroot/js/tutorial.jsx
+++ ReactDemo/wwwroot/js/tutorial.jsx
@@ -1,8 +1,103 @@
 ﻿const data = [
     {id:1,author:'donnayy',text:'so truuu'},
     {id:2,author:'monnayy',text:'so mruuu'},
-    {id:1,author:'fonnayy',text:'so fsruuu'},
+    {id:3,author:'fonnayy',text:'so fsruuu'},
 ];
+
+class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+        this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    }
+    loadCommentsFromServer() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    }
+    handleCommentSubmit = comment => {
+        const data = new FormData();
+        data.append('author', comment.author);
+        data.append('text', comment.text);
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('post', this.props.submitUrl, true);
+        xhr.onload = function () {
+            this.loadCommentsFromServer();
+        }.bind(this);
+        xhr.send(data);
+    }
+    componentDidMount() {        
+        window.setInterval(this.loadCommentsFromServer(), this.props.pollInterval);
+    }
+
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+            </div>
+        );
+    }
+}
+
+class CommentList extends React.Component {
+    render() {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment >
+            );
+        });
+        return <div className="commentList">{commentNodes}</div>;
+    }
+}
+
+class CommentForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { author: '', text: '' };
+        this.handleAuthorChange = this.handleAuthorChange.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    state = {
+        author: '',
+        text: ''
+    };
+    handleAuthorChange = e => {
+        this.setState({ author: e.target.value });
+    }
+    handleTextChange = e => {
+        this.setState({ text: e.target.value });
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if (!text || !author) {
+            return;
+        }
+        this.props.onCommentSubmit({ author: author, text: text })
+        this.setState({ author: '', text: '' });
+    }
+    render() {
+        return (
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
+                <input type="text" placeholder="Say something" value={this.state.text} onChange={this.handleTextChange} />
+                <input type="submit" value="Post" />
+            </form>
+        );
+    }
+}
 
 function createRemarkable() {
     var remarkable =
@@ -13,11 +108,12 @@ function createRemarkable() {
     return new remarkable();
 }
 class Comment extends React.Component {
-    rawMarkup() {
-        const md = new Remarkable();
-        const rawMarkup = md.render(this.props.children.toString());
+    rawMarkup = () => {
+        var md = createRemarkable();
+        var rawMarkup = md.render(this.props.children.toString());
         return { __html: rawMarkup };
-    }
+    };
+
     render() {
         return (
             <div className="comment">
@@ -25,38 +121,10 @@ class Comment extends React.Component {
                 <span dangerouslySetInnerHTML={this.rawMarkup()}/>
             </div>
         );
-    }
+    };
 }
 
-class CommentBox extends React.Component {
-    render() {
-        return (
-            <div className="commentBox">
-                <h1>Comments</h1>
-                <CommentList data={this.props.data} />
-                <CommentForm />
-            </div>
-        );
-    }
-}
-
-class CommentList extends React.Component {
-    render() {
-        const commentNodes = this.props.data.map(comment => (
-            <Comment author={comment.author} key={comment.id}>
-                {comment.text}
-            </Comment >
-        ));
-        return <div className="commentList">{commentNodes}</div>;
-    }
-}
-
-class CommentForm extends React.Component {
-    render() {
-        return (
-            <div className="commentForm">commentForm</div>
-        );
-    }
-}
-
-ReactDOM.render(<CommentBox data={data} />, document.getElementById('content'));
+ReactDOM.render(
+    <CommentBox url="/comments" submitUrl="/comments/new" pollInterval={2000} />,
+    document.getElementById('content')
+);
